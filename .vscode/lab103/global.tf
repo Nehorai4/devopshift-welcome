3c1e734afcc89b552e7b630dafa6afa5bf9862af
Your Name <you@example.com>
2025-02-05 13:48:43 +0000

    lab 103

---
+++ .vscode/lab103/global.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+ region = var.region
+}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+variable "ami" {
+ default = "ami-0ecc0e0d5986a576d"
+ }
+variable "vm_name" {
+ default = "vm-Nehorai"
+}
+
+variable "admin_username" {
+ default = "admin-user"
+}
+
+variable "admin_password" {
+ default = "Password123!"
+}
+
+variable "vm_size" {
+ default = "t2.micro"
+}
